adb093b73f39dc2826aa744186304df7b3eddfad
CeezR <cesarforthomme@gmail.com>
2023-09-02 17:40:43 +0200

    feat: succesfule implementation of TreeChart

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,11 +1,35 @@
 import D3Test from "./D3Test";
 import LinePlot from "./LinePlot";
 import RoadMap from "./RoadMap";
+import TreeChart from "./TreeChart";
+
+const initialData = {
+  name: "😐",
+  children: [
+    {
+      name: "🙂",
+      children: [
+        {
+          name: "😀"
+        },
+        {
+          name: "😁"
+        },
+        {
+          name: "🤣"
+        }
+      ]
+    },
+    {
+      name: "😔"
+    }
+  ]
+};
 
 export default function Home() {
   return (
     <main>
-      <D3Test/>
+      <TreeChart data={initialData} />
     </main>
   )
 }
